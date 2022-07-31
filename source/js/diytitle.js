0468cf92181eeea9039d76e65adcc8867fc61e14
huang <2867153000@qq.com>
2022-07-31 19:54:30 +0800

    '第二次提交'

---
+++ source/js/diytitle.js
@@ -0,0 +1,18 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = '(；OдO) 不要走！再看看嘛！';
+        clearTimeout(titleTime);
+    }
+    else {
+        //返回当前页面时标签显示内容
+        document.title = '( • ̀ω•́ )✧哎呀!被发现了~' + OriginTitile;
+        //两秒后变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
